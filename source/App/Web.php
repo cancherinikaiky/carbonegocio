1bc067d23d35e8b97b7c4907f524a3b031052948
vinicius <vinicius.shr@gmail.com>
2022-11-29 05:00:02 -0300

    Merge branch 'main' of https://github.com/cancherinikaiky/tecstart

--- source/App/Web.php
+++ source/App/Web.php
@@ -2,10 +2,12 @@
 
 namespace Source\App;
 
+require "./source/autoload.php";
 use League\Plates\Engine;
 use Source\Models\Category;
 use Source\Models\Client;
 use Source\Models\Worker;
+use Source\Support\ValidateInputs;
 
 class Web {
     private $view;
@@ -46,17 +48,21 @@ class Web {
         echo $this->view->render("register",["eventName" => CONF_SITE_NAME]);
     }
 
-    public function postRegisterClient(?array $data): void {
-        $cliente = new Client(
-            null,
-            "jukão",
-            "do trago",
-            "11111111111",
-            "jukinha@gmail.com",
-            "12345678"
-        );
-
-        $cliente->create();
+    public function postRegisterClient(): void {
+        header('Content-Type: application/json;');
+        $validate = new ValidateInputs();
+        if($validate->Inputs()) {
+            $client = new Client(
+                NULL,
+                $_POST['name'],
+                $_POST['lastName'],
+                $_POST['cpf'],
+                $_POST['email'],
+                $_POST['password']
+            );
+            echo json_encode("inserido com sucesso");
+            $client->create();
+        }
     }
 
     public function getLoginRender(?array $data): void {
--- source/Support/Validate.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Source\Support;
-
-class Validate
-{
-
-}
